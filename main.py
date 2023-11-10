cbade7e7c5ffc262ee6181b2ea4c4ed63f05af24
joanatroplini <joana.troplini@hotmail.com>
2023-11-10 23:32:03 +0100

    create a pdf from text files, added text for each page

--- main.py
+++ main.py
@@ -8,18 +8,28 @@ filepaths = glob.glob("text/*.txt")
 
 def read_txt(filepath):
     with open (filepath, "r") as fp:
-        content = fp.read()
-    return content
+        txt_content = fp.read()
+    return txt_content
 
 
+# Create pdf
 pdf = FPDF(orientation="P", unit="mm", format="A4")
 
 for filepath in filepaths:
-    content = read_txt(filepath)
     filename = Path(filepath).stem.title()
+    # Add one page per each text file
     pdf.add_page()
-    pdf.set_font(family="Times", size=18, style="B")
+    # Add Title of the page
+    pdf.set_font(family="Times", size=16, style="B")
     pdf.cell(w=50, h=8, txt=filename, ln=1)
+
+    # get content and add it to the pdf page
+    content = read_txt(filepath)
+    pdf.set_font(family="Times", size=12)
+
+    # Height corresponds to the height of each cell
+    # from the multicell
+    pdf.multi_cell(w=0, h=6, txt=content)
 
 pdf.output("output.pdf")
 
